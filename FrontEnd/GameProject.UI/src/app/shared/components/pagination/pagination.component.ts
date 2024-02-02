286d995ea0a75673eed775c8506952103cb7dece
Vitalii Novik <novickvitaliy@gmail.com>
2024-02-02 21:46:42 +0200

    Refactored pagination

--- FrontEnd/GameProject.UI/src/app/shared/components/pagination/pagination.component.ts
+++ FrontEnd/GameProject.UI/src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -9,10 +9,58 @@ export class PaginationComponent {
   @Input({required: true}) totalItemsCount: number;
   @Input({required: true}) itemsPerPage: number;
   @Input({required: true}) currentPage: number;
+  @Input({required: true}) collectionSize = 0;
   @Output() pageChanged: EventEmitter<number> = new EventEmitter<number>();
   protected readonly Math = Math;
 
   nextPage(pageNumber: number){
     this.pageChanged.emit(pageNumber);
+  }
+
+  /** The number of buttons to show either side of the current page */
+  @Input()
+  maxSize = 2;
+
+  /** Display the First/Last buttons */
+  @Input()
+  firstLastButtons = false;
+
+  /** Display the Next/Previous buttons */
+  @Input()
+  nextPreviousButtons = true;
+
+  /** Display small pagination buttons */
+  @Input()
+  small = false;
+
+  totalPages: any[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.totalPages = new Array(Math.ceil(this.collectionSize / this.itemsPerPage));
+  }
+
+  /** Update totalPage number if the collectionSize or pageSize values change */
+  ngOnChanges(changes: SimpleChanges) {
+    this.totalPages = new Array(Math.ceil(this.collectionSize / this.itemsPerPage));
+  }
+
+  /** Set page number */
+  selectPageNumber(pageNumber: number) {
+    this.currentPage = pageNumber;
+    this.pageChanged.emit(this.currentPage);
+  }
+
+  /** Set next page number */
+  next() {
+    const nextPage = this.currentPage + 1;
+    nextPage <= this.totalPages.length && this.selectPageNumber(nextPage);
+  }
+
+  /** Set previous page number */
+  previous() {
+    const previousPage = this.currentPage - 1;
+    previousPage >= 1 && this.selectPageNumber(previousPage);
   }
 }
